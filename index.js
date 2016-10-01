866549c0b2dbebce68783e1daceb0e4411840dc1
Michael LoPiccolo <michael@puck>
2016-10-01 03:17:17 -0500

    let's make it work

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send("official ranking of milos: <br> 1. milo the rapper<br> 2. me<br> 3. other milos<br><br><br>-inf. milo yiannopoulos")
+  console.log('served a request!')
 })
+
+
+express.listen(80, function()) {
+  console.log('starting up!')
+}
