60afd7d23e2c6823599c27a5df47e780b28ed168
Michael LoPiccolo <m.lopiccolo3@gmail.com>
2016-10-02 04:16:53 -0400

    fix it whee

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send("official ranking of milos: <br> 1. milo the rapper<br> 2. me<br> 3. other milos<br><br><br>-inf. milo yiannopoulos")
-  console.log('served a request!')
-})
+  res.send("official ranking of milos: <br> 1. milo the rapper<br> 2. me<br> 3. other milos<br><br><br>-inf. milo yiannopoulos");
+  console.log('served a request!');
+});
 
 
-express.listen(80, function()) {
-  console.log('starting up!')
-}
+app.listen(80, function() {
+  console.log('starting up!');
+});
