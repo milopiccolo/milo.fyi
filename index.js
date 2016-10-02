08a4fa1dbd5bce6104951040ac95127d4c68ea99
Michael LoPiccolo <michael@puck>
2016-10-02 13:05:10 -0500

    Use infinity symbol

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
-var port = (process.argv.length >= 2 ? process.argv[2] : 80);
+var port = (process.argv.length >= 3 ? process.argv[2] : 3000);
 console.log(port);
 
 app.get('/', function(req, res) {
-  res.send("official ranking of milos: <br> 1. milo the rapper<br> 2. me<br> 3. other milos<br><br><br>-inf. milo yiannopoulos");
+  res.send("official ranking of milos: <br> 1. milo the rapper<br> 2. me<br> 3. other milos<br><br><br>-&#8734;. milo yiannopoulos");
   console.log('served a request!');
 });
 
 
 app.listen(port, function() {
-  console.log('starting up!');
+  console.log('starting up on port ' + port + '!');
 });
