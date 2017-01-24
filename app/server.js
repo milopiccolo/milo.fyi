dd4fd30000755fd116d0e372594e64899e1bc435
Michael LoPiccolo <m.lopiccolo3@gmail.com>
2017-01-24 17:28:55 -0600

    Wrote a temporary page to host things while I'm busy!

--- app/server.js
+++ app/server.js
@@ -5,10 +5,12 @@ console.log(port);
 
 app.use(express.static('../dist'));
 
-app.get('/', function(req, res) {
-  res.send('official ranking of milos: <br> 1. milo the rapper<br> 2. me<br> 3. other milos<br><br><br>-&#8734;. milo yiannopoulos<br><br> under construction, contact <a href="mailto:me@milo.fyi">me@milo.fyi</a> for issues');
-  console.log('served a request!');
-});
+// app.get('/', function(req, res) {
+  // res.send('Michael LoPiccolo<br><br> I\'ll make a real website sometime, but until then:<br> <a href="static/">My paper reviewing fairness & nondiscrimination efforts in big data</a><br><a href="');
+    //'I hope to ');
+  //res.send('official ranking of milos: <br> 1. milo the rapper<br> 2. me<br> 3. other milos<br><br><br>-&#8734;. milo yiannopoulos<br><br> under construction, contact <a href="mailto:me@milo.fyi">me@milo.fyi</a> for issues');
+  // console.log('served a request!');
+// });
 
 app.listen(port, function() {
   console.log('starting up on port ' + port + '!');
