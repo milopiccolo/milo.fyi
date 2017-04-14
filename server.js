235dbdcbc3dbcc7bc43d7acc776f3d5daf0c10d2
Michael LoPiccolo <m.lopiccolo3@gmail.com>
2017-04-14 17:50:30 -0500

    Hamming checker ready to roll!

--- server.js
+++ server.js
@@ -3,20 +3,13 @@ var app = express();
 var port = (process.argv.length >= 3 ? process.argv[2] : 3000);
 console.log(port);
 
-var bodyParser = require('body-parser');
-app.use(bodyParser());
+var multer = require('multer');
+
 
 app.use(express.static(__dirname+'/dist',{extensions:['html']}));
 
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
-
-// app.get('/', function(req, res) {
-  // res.send('Michael LoPiccolo<br><br> I\'ll make a real website sometime, but until then:<br> <a href="static/">My paper reviewing fairness & nondiscrimination efforts in big data</a><br><a href="');
-    //'I hope to ');
-  //res.send('official ranking of milos: <br> 1. milo the rapper<br> 2. me<br> 3. other milos<br><br><br>-&#8734;. milo yiannopoulos<br><br> under construction, contact <a href="mailto:me@milo.fyi">me@milo.fyi</a> for issues');
-  // console.log('served a request!');
-// });
 
 app.listen(port, function() {
   console.log('starting up on port ' + port + '!');
@@ -31,24 +24,55 @@ var fs = require('fs');
 var runid = 0;
 var escape = require('escape-html');
 
+app.get('/hamming', function(req, res) {
+  res.render('hamming');
+});
+
+var upload = multer({
+  dest: "tmp/",
+  limits: {
+    fileSize: 1000 * 1024 * 1024, // TODO check this
+    fields: 2,
+    fieldSize: 1000 * 1024 * 1024,
+    files: 1
+  }
+});
+
+app.use('/hamming', upload.any());
 app.post('/hamming', function(req, res) {
-  var output = "";
-  var file = __dirname+'/tmp/'+runid;
-  fs.writeFile(file, req.body.data);
-  var command = __dirname+'/progs/hamming ' + file;
-  runid++;
-  if(req.body.distance && /[0-9]*/.test(req.body.distance)) {
-    command += " -d " + req.body.distance;
+
+  console.log(req.body);
+
+  var filename;
+  
+  var out = '';
+  if(req.files.length==0) {
+    filename = 'tmp/'+runid;
+    fs.writeFile(filename, req.body.text);
+    runid++;
+  }
+  else {
+    filename = req.files[0].path;
+    out += "Reading from file...\n"
+  }
+  var command = __dirname+'/progs/hamming ' + filename;
+  if(req.body.maxhd) {
+    command += ' -d' + req.body.maxhd;
   }
   exec(command, (error, stdout, stderr) => {
-    var out = "";
     if(error) {
-      out = "There was an error. Output:\n\n" + stdout;
+      out += "There was an error. Output:\n\n" + stdout;
     }
     else {
-      out = stdout;
+      out += stdout;
     }
     var eout = escape(out).replace(/\n/g, "<br>");
     res.render('hamming', {escapedoutput:eout});
+
+    exec("rm " + filename);
+    for(i in req.files) {
+      var rmcommand = "rm " + req.files[i].path;
+      exec(rmcommand);
+    }
   });
 });
